e457ef341138a4e2f063e957d541a792d4c0f30b
hanseoryu <hanseo.ryu@gmail.com>
2026-05-23 23:18:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const CACHE = 'health-diary-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'health-diary-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
